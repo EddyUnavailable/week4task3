239d179b5a37910d970743e098966c63b03cfdda
eddy-sullivan <fleatoad@hotmail.com>
2025-02-04 02:34:37 +0000

    another try

--- server/index.js
+++ server/index.js
@@ -1,45 +1,59 @@
 import express from "express"
-const app = document.getElementById('app')
+import { hostname } from "os";
 
-async function fetchData() {
-  const res = await fetch(`http://localhost:5173/jokes`)
-  const jokes = await res.json()
+const http = require(`http`);
 
-  displayJokes(jokes)
-}
 
-function displayJokes(param) {
-  // clear the div before we add things 
-  app.innerHTML = ''
-  param.forEach(singleJoke => {
-    const h3 = document.createElement('h3')
-    const pTag = document.createElement('p')
-    const div = document.createElement('div')
-    const deleteButton = document.createElement('button')
+const port = process.env.port || 5000;
+const server = http.createServer(app);
 
-    h3.innerText = singleJoke.joke
-    pTag.innerText = singleJoke.punchline
-    deleteButton.innerText = 'X'
+server.listen(port, hostname () => {
+  console.log(`started on port ${port}`);
+});
 
-    deleteButton.addEventListener('click', function() {
-      handleDelete(singleJoke.id)
-    })
 
-    div.appendChild(deleteButton)
-    div.appendChild(h3)
-    div.appendChild(pTag)
 
-    app.appendChild(div)
-  })
-}
+// const app = document.getElementById('app')
 
-fetchData()
+// async function fetchData() {
+//   const res = await fetch(`http://localhost:5173/jokes`)
+//   const jokes = await res.json()
 
-async function handleDelete(id) {
-  const res = await fetch(`http://localhost:5173/jokes/${id}`, {
-    method: 'DELETE'
-  })
-  if (res.ok) {
-    fetchData()
-  }
-}
+//   displayJokes(jokes)
+// }
+
+// function displayJokes(param) {
+//   // clear the div before we add things 
+//   app.innerHTML = ''
+//   param.forEach(singleJoke => {
+//     const h3 = document.createElement('h3')
+//     const pTag = document.createElement('p')
+//     const div = document.createElement('div')
+//     const deleteButton = document.createElement('button')
+
+//     h3.innerText = singleJoke.joke
+//     pTag.innerText = singleJoke.punchline
+//     deleteButton.innerText = 'X'
+
+//     deleteButton.addEventListener('click', function() {
+//       handleDelete(singleJoke.id)
+//     })
+
+//     div.appendChild(deleteButton)
+//     div.appendChild(h3)
+//     div.appendChild(pTag)
+
+//     app.appendChild(div)
+//   })
+// }
+
+// fetchData()
+
+// async function handleDelete(id) {
+//   const res = await fetch(`http://localhost:5173/jokes/${id}`, {
+//     method: 'DELETE'
+//   })
+//   if (res.ok) {
+//     fetchData()
+//   }
+// }
