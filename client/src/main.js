580a0c838ec2dcfd40dd464b18b916cced705fe1
eddy-sullivan <fleatoad@hotmail.com>
2025-02-04 01:20:13 +0000

    still broke

--- client/src/main.js
+++ client/src/main.js
@@ -53,78 +53,84 @@ const userName = document.querySelector('#user')
 const comment = document.querySelector('#comment')
 const commentsCont = document.querySelector('.comments__container')
 
-submitBtn.addEventListener('click', submitFeedback)
-
-function submitFeedback(e){
-    // get user name
-    const userForm = userName.value 
-    // get feedback
-    const commentForm = comment.value 
-    // if inputs are not empty
-    if(userForm && commentForm !== ''){
-        // create new feedback
-        newFeedback = {
-            "id": Math.floor((Math.random() * 1000)+ 1),
-            "userName": userForm,
-            "userComment": commentForm,
-            "typeOfFeedback": positiveFeedback
-        }
-        // add new feedback to array
-        feedbackArr.push(newFeedback)
-        // if liked add to count
-        if(positiveFeedback === true){
-            addLikes()
-        }
-        // clear inputs 
-        resetForm()
-        // add feedback to list
-        addFeedback(newFeedback)
-    }
-
-
-    e.preventDefault()
-}
 
 
 
-function resetForm(){
-    userName.value = ''
-    comment.value = ''
-    likeIcon.innerHTML = `<i class="far fa-heart"></i>`
-    positiveFeedback = false
-}
+      submitBtn.addEventListener('click', function () {
+        const userName_text = userName.value;
+        const comment_text = comment.value;
+        targetField_userName.innerHTML = userName_text;
+        targetField_comment.innerHTML = comment_text;
+      });
 
-function addFeedback(item){
-    // select first letter of the user name
-    const letter = (item.userName).charAt(0)
-    // create new div
-    const div = document.createElement('div')
-    // add class
-    div.classList = 'comment__card'
-    // add id
-    div.id = item.id 
-    // add html
-    div.innerHTML = `
-    <div class="pic center__display">
-                    ${letter}
-                </div>
-                <div class="comment__info">
-                    <small class="nickname">
-                        ${item.userName}
-                    </small>
-                    <p class="comment">
-                        ${item.userComment}
-                    </p>
-                    <div class="comment__bottom">
-                        <div class="heart__icon--comment">
-                            ${item.typeOfFeedback ? `<i class="fas fa-heart positive"></i>` : `<i class="far fa-heart"></i>`}
-                        </div>
-                        <button>
-                            Reply
-                        </button>
-                    </div>
-                </div>
-    `
-    // insert feedback into the list
-    commentsCont.insertAdjacentElement('beforeend', div)
-}
+
+// function submitFeedback(e){
+//     // get user name
+//     const userForm = userName.value 
+//     // get feedback
+//     const commentForm = comment.value 
+//     // if inputs are not empty
+//     if(userForm && commentForm !== ''){
+//         // create new feedback
+//         newFeedback = {
+//             "id": Math.floor((Math.random() * 1000)+ 1),
+//             "userName": userForm,
+//             "userComment": commentForm,
+//             "typeOfFeedback": positiveFeedback
+//         }
+//         // add new feedback to array
+//         feedbackArr.push(newFeedback)
+//         // if liked add to count
+//         if(positiveFeedback === true){
+//             addLikes()
+//         }
+//         // clear inputs 
+//         resetForm()
+//         // add feedback to list
+//         addFeedback(newFeedback)
+//     }
+
+//     e.preventDefault()
+// }
+
+// function resetForm(){
+//     userName.value = ''
+//     comment.value = ''
+//     likeIcon.innerHTML = `<i class="far fa-heart"></i>`
+//     positiveFeedback = false
+// }
+
+// function addFeedback(item){
+//     // select first letter of the user name
+//     const letter = (item.userName).charAt(0)
+//     // create new div
+//     const div = document.createElement('div')
+//     // add class
+//     div.classList = 'comment__card'
+//     // add id
+//     div.id = item.id 
+//     // add html
+//     div.innerHTML = `
+//     <div class="pic center__display">
+//                     ${letter}
+//                 </div>
+//                 <div class="comment__info">
+//                     <small class="nickname">
+//                         ${item.userName}
+//                     </small>
+//                     <p class="comment">
+//                         ${item.userComment}
+//                     </p>
+//                     <div class="comment__bottom">
+//                         <div class="heart__icon--comment">
+//                             ${item.typeOfFeedback ? `<i class="fas fa-heart positive"></i>` : `<i class="far fa-heart"></i>`}
+//                         </div>
+//                         <button>
+//                             Reply
+//                         </button>
+//                     </div>
+//                 </div>
+//     `
+//     // insert feedback into the list
+//     commentsCont.insertAdjacentElement('beforeend', div)
+// }
